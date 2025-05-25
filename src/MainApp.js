baa45ffdc09117f78506088bbfdcf9ca30342b3a
Chandresh Kumar <chandresh.code@gmail.com>
2025-05-25 12:40:23 +0530

    safe area provider fix

--- src/MainApp.js
+++ src/MainApp.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { enableScreens } from 'react-native-screens';
 import { Provider as PaperProvider } from 'react-native-paper';
 import AppTheme from './theme/AppTheme'; // Import the custom theme
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import StoreProvider from './redux/StoreProvider.component';
 import GameScreen from './screens/GameScreen.component';
@@ -10,11 +11,13 @@ enableScreens();
 
 const MainApp = () => {
     return (
-        <PaperProvider theme={AppTheme}>
-            <StoreProvider>
-                <GameScreen />
-            </StoreProvider>
-        </PaperProvider>
+        <SafeAreaProvider>
+            <PaperProvider theme={AppTheme}>
+                <StoreProvider>
+                    <GameScreen />
+                </StoreProvider>
+            </PaperProvider>
+        </SafeAreaProvider>
     );
 };
 
