6420159055b91be5829cbed035de6d678d3df699
Chandresh Kumar <chandresh.code@gmail.com>
2026-02-07 10:04:21 +0000

    chore: upgrade to Expo SDK 52 and switch to managed workflow
    Upgrade from Expo SDK 48 to SDK 52 (React 18.3.1, RN 0.76.7). Migrate
    from legacy Redux to Redux Toolkit, remove deprecated packages (redux,
    redux-devtools-extension, @expo/webpack-config), and delete the stale
    android/ directory to return to managed Expo workflow where EAS Build
    generates native projects automatically.

--- src/redux/StoreProvider.component.jsx
+++ src/redux/StoreProvider.component.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import { legacy_createStore as createStore } from 'redux';
-// import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './root-reducer';
 
-const store = __DEV__
-    ? createStore(rootReducer, composeWithDevTools())
-    : createStore(rootReducer);
-// const store = configureStore({
-//     reducer: rootReducer,
-//     devTools: __DEV__ ? composeWithDevTools() : false,
-// });
+const store = configureStore({
+    reducer: rootReducer,
+    devTools: __DEV__,
+});
 
 const StoreProvider = ({ children }) => {
     return <Provider store={store}>{children}</Provider>;
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,4 +1,4 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from '@reduxjs/toolkit';
 
 import gameReducer from './reducers/game.reducer';
 
